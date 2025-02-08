b6a2290d90b25e79ed1a28354d44b475b253230c
NCG77 <swastikmishra7799@gmail.com>
2025-02-08 23:26:36 +0530

    Updates the looks for the main page

--- src/Components/Home_Page/HomePage.tsx
+++ src/Components/Home_Page/HomePage.tsx
@@ -5,21 +5,18 @@ import MicIcon from './../../Assets/Mic-Image.png';
 
 const Mainpage: React.FC = () => {
     const [activePage, setActivePage] = useState('home');
-    const [isRecording, setIsRecording] = useState(false); // State for recording status
+    const [isRecording, setIsRecording] = useState(false);
 
     const handleNavigation = (page: string) => {
         setActivePage(page);
     };
 
     const handleRecordClick = () => {
-        setIsRecording(!isRecording); // Toggle recording state
-        // Add your recording logic here (start/stop recording, etc.)
+        setIsRecording(!isRecording);
         if (isRecording) {
             console.log("Stopping recording...");
-            // Stop recording
         } else {
             console.log("Starting recording...");
-            // Start recording
         }
     };
 
@@ -39,17 +36,15 @@ const Mainpage: React.FC = () => {
                 <div className="hero-content">
                     <h1>Bespoke: Your AI-Powered Healthcare Companion</h1>
                     <p>Revolutionizing healthcare with intelligent voice analysis and prescription management.</p>
-                    <button className="cta-button">Get Started</button>
                 </div>
-                <div className="hero-buttons"> {/* Container for the hero buttons */}
-                    <button className="hero-action-button">Voice Analysis</button>
-                    <button className="hero-action-button">Prescription Management</button>
-                    <button className="hero-action-button">AI Insights</button>
+                <div className="hero-buttons">
+                    <button className="hero-action-button-left">Voice Analysis</button>
                     <button className="mic-button" onClick={handleRecordClick}>
                         <img src={MicIcon} alt="Microphone" className="mic-icon" />
                     </button>
+                    <button className="hero-action-button-right">Prescription Management</button>
                 </div>
-            </section>
+            </section >
 
             <footer className="footer">
                 <div className="footer-bottom">
@@ -60,7 +55,7 @@ const Mainpage: React.FC = () => {
                     </ul>
                 </div>
             </footer>
-        </div>
+        </div >
     );
 };
 
